7f343870302f08f8e72af79c554681e74c903c1e
loucman <loucman.machababy@epitech.eu>
2025-05-30 00:56:17 +0200

    create my route register

--- my_quiz/src/Controller/AuthController.php
+++ my_quiz/src/Controller/AuthController.php
@@ -9,3 +9,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class AuthController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $em): Response
+    {
+    }}
