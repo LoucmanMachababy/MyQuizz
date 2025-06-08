62e3070713e62c62e892ec73b7002c9137d8eb87
loucman <loucman.machababy@epitech.eu>
2025-06-08 13:33:02 +0200

    create route respond to account/mail and use get and post method

---
+++ my_quiz/src/Controller/AccountController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Uid\Uuid;
+
+class AccountController extends AbstractController
+{
+    #[Route('/account/email', name: 'account_email')]
+
+}
