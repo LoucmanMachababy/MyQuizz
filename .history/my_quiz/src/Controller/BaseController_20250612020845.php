9ee584c9e692de02b44a097c8e8ae8076db61140
Safiya Jazouli <safiya.jazouli@epitech.eu>
2025-06-12 16:00:01 +0200

    css and symfony auth

---
+++ .history/my_quiz/src/Controller/BaseController_20250612020845.php
@@ -0,0 +1,22 @@
+// src/Controller/BaseController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+
+class BaseController extends AbstractController
+{
+    protected function getLoggedUser(Request $request, EntityManagerInterface $em): ?User
+    {
+        $session = $request->getSession();
+        $userId = $session->get('user_id');
+
+        if (!$userId) {
+            return null;
+        }
+
+        return $em->getRepository(User::class)->find($userId);
+    }
+}
---
+++ .history/my_quiz/src/Entity/User_20250612010655.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+#[ORM\Entity(repositoryClass: "App\Repository\UserRepository")]
+#[ORM\Table(name: "user")]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "integer")]
+    #[ORM\GeneratedValue]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 255, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private $password;
+
+    #[ORM\Column(type: "boolean", name: "email_confirmed")]
+    private $emailConfirmed = false;
+
+    #[ORM\Column(type: "string", length: 64, nullable: true)]
+    private $confirmationToken;
+
+    #[ORM\Column(type: "json")]
+    private array $roles = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function isEmailConfirmed(): bool
+    {
+        return $this->emailConfirmed;
+    }
+
+    public function setEmailConfirmed(bool $confirmed): self
+    {
+        $this->emailConfirmed = $confirmed;
+        return $this;
+    }
+
+    public function getConfirmationToken(): ?string
+    {
+        return $this->confirmationToken;
+    }
+
+    public function setConfirmationToken(?string $token): self
+    {
+        $this->confirmationToken = $token;
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function eraseCredentials(): void
+    {
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+}
